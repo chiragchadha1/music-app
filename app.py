839bb1d8f6340802b806a10b22f18090e65d6d47
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-18 17:45:59 -0400

    testing sql

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = '6kWyqokwLRHqy6HyIvKC'
+app.config['MYSQL_DATABASE_HOST'] = 'musicapp.cf3u0flvivkp.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_DB'] = 'musicApp'
+conn = mysql.connect()
+cursor =conn.cursor()
+
+cursor.execute("SELECT * from User")
+data = cursor.fetchone()
+mysql.init_app(app)
 
 @app.route("/")
 def hello():
-    return "wumbology"
+    return data
 
 if __name__ == "__main__":
     app.run()
