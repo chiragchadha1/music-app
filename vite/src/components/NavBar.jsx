5a290b97db93c9e2bf94180f8753ed46c1bbf1f3
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-19 11:18:16 -0400

    Fixed mobile navbar

--- vite/src/components/NavBar.jsx
+++ vite/src/components/NavBar.jsx
@@ -1,28 +1,44 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+// import { useState } from 'react';
+import {Nav, Navbar, Container} from 'react-bootstrap'
 
 function NavBar() {
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Music App</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link active" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link active" to="/signup">Sign Up</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link active" to="/login">Login</Link>
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Music App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-toggle" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/signup">Sign Up</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+        // <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        //     <div className="container-fluid">
+        //         <Link className="navbar-brand" to="/">Music App</Link>
+        //         <button className="navbar-toggler" type="button" onClick={() => toggleMenu}>
+        //         <span className="navbar-toggler-icon"></span>
+        //         </button>
+        //         <div className="collapse navbar-collapse" id="navbarNav">
+        //         <ul className="navbar-nav">
+        //             <li className="nav-item">
+        //                 <Link className="nav-link active" to="/">Home</Link>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <Link className="nav-link active" to="/signup">Sign Up</Link>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <Link className="nav-link active" to="/login">Login</Link>
+        //             </li>
+        //         </ul>
+        //         </div>
+        //     </div>
+        // </nav>
     );
 }
 
