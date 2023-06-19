6e53c6bf94d4646ca4eea3cd9b6a3bebaf977968
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-19 11:25:55 -0400

    testing links

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,21 +1,19 @@
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 
 function App() {
     return (
-        <Router>
-            <div className='container'>
-                <NavBar />
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/" element={<Home/>} />
-                </Routes>
-            </div>
-        </Router>
+        <div className='container'>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="login" element={<Login />} />
+            </Routes>
+        </div>
     );
 }
 
