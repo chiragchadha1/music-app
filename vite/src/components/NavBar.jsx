cc69f4f6773442a5e043d089665a6ceeee990dbe
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-19 11:53:18 -0400

    trying react-router-bootstrap

--- vite/src/components/NavBar.jsx
+++ vite/src/components/NavBar.jsx
@@ -1,44 +1,27 @@
-// import { Link } from 'react-router-dom'
-// import { useState } from 'react';
-import {Nav, Navbar, Container} from 'react-bootstrap'
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function NavBar() {
-
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">Music App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-toggle" />
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-        // <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        //     <div className="container-fluid">
-        //         <Link className="navbar-brand" to="/">Music App</Link>
-        //         <button className="navbar-toggler" type="button" onClick={() => toggleMenu}>
-        //         <span className="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className="collapse navbar-collapse" id="navbarNav">
-        //         <ul className="navbar-nav">
-        //             <li className="nav-item">
-        //                 <Link className="nav-link active" to="/">Home</Link>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <Link className="nav-link active" to="/signup">Sign Up</Link>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <Link className="nav-link active" to="/login">Login</Link>
-        //             </li>
-        //         </ul>
-        //         </div>
-        //     </div>
-        // </nav>
+            <Container>
+                <Navbar.Brand href="/">Music App</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-toggle" />
+                <Navbar.Collapse id="navbar-toggle">
+                    <Nav className="me-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/signup">
+                            <Nav.Link>Sign Up</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
