1c6a07b1710170541d9fc8c535cec9ded4ebf216
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-19 23:32:10 -0400

    tesitng

--- api/server.py
+++ api/server.py
@@ -48,11 +48,11 @@ def protected():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
-
+     path_dir = os.path.abspath("../vite/dist") #path react build
+     if path != "" and os.path.exists(os.path.join(path_dir, path)):
+         return send_from_directory(os.path.join(path_dir), path)
+     else:
+         return send_from_directory(os.path.join(path_dir),'index.html')
 
 @app.route('/api/signup', methods=['POST'])
 def signup():
